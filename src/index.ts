6aa70f56dd31a358b406b7714658714cdd32f2aa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-17 12:26:06 +0000

    Deploy gen-tech/scroll-manager to github.com/gen-tech/scroll-manager.git:gh-pages

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import { ScrollManager } from "./scroll-manager";
-
-export { ScrollManager, IScrollToElementOptions } from "./scroll-manager";
-export { ScrollObserver, ScrollDirection, ScrollPhase, TScrollableContent } from "./scroll-observer";
-
-export default new ScrollManager();
